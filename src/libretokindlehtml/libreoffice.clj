81b246d495ce1f46409cf5f687ea69b162ec8095
trisdan <tsleyson@ucdavis.edu>
2013-12-03 23:45:57 -0800

    Nothing in ofnight.clj was specific to Of Night, so renamed to libreoffice.clj.

--- src/libretokindlehtml/libreoffice.clj
+++ src/libretokindlehtml/libreoffice.clj
@@ -1,6 +1,6 @@
-(ns libretokindlehtml.ofnight
+(ns libretokindlehtml.libreoffice
   "Defines snippets and templates specifically for the conversion
-   from Libre Office html to nice html for the book Of Night."
+   from Libre Office html to nice html."
   (require [net.cgrand.enlive-html :refer :all]
            [clojure.java.io :refer [file]]
            [clojure.pprint :refer [pprint]]))
@@ -13,12 +13,18 @@
 ; Template: The final template.
 
 ; cleanup functions.
-(defn remove-br-in-p
-  "Removes any <br> tag inside a <p> tag." ; with a very elispy name.
-  [page]
-  (transform [:p :br] nil))
+
+(defn libre-maid
+  "Runs a suite of cleanup functions on the given paragraph."
+  []
+  (transformation 
+   [:p :br] nil ; get rid of all br inside p tags.
+   [[:span (but (or (attr? :class) (attr? :id)))]] 
+    unwrap ; unwrap pointless spans at top of paragraph.
+   ))
 
 ; snippets here.
+
 (defsnippet chapter (file "resources/ofnight/chaptersnip.html") [:div.chapter]
   [[header & body] & cleanup]
   [:span.chapheading] (let [cname (first (select header [[:h3 first-of-type] :> text-node]))]
@@ -26,14 +32,10 @@
                              ; all the crap and get just the text node.
                         (html-content (str "<a id='" cname "'>" cname "</a>")))
   [:div#chaptertext :p.standard]  (clone-for [para body]
-                                             [:p.standard] (content ((if cleanup
-                                                                       (apply comp cleanup)
-                                                                       identity)
-                                                                     (para :content)))))
+                                             [:p] (content (cleanup (para :content)))))
 
-; cleanup is a collection of functions to clean up the paragraph text. In this case
+; cleanup is a function to clean up the paragraph text. In this case
 ; we'll probably want to unnest from those pointless spans and get rid of all the
-; pointless brs inside the paragraphs. Since I couldn't figure out any nicer way to do
-; it (using a let wasn't really nicer), I used an if that returns the composition of
-; all the cleanup functions if the cleanup argument has been passed, and otherwise uses
-; the identity function.
+; pointless brs inside the paragraphs.
+; Also note that I had to select :p under the clone-for because I used to have :p.standard
+; but you're actually selecting from the resource, not the template.
